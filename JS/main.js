87882febfa4a1eb995b10d1a63f0542d892eaa9c
ThekoengKG <kg.thekoeng@gmail.com>
2021-05-19 15:47:27 +0200

    Blog project

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+const formInputs = document.querySelectorAll(".form-input");
+
+ formInputs.forEach((formInput) => {
+     let thisLabel = formInput.nextElementSibling;
+
+     formInput.addEventListener("focus", () => {
+         thisLabel.classList.add("active");
+     })
+
+     formInput.addEventListener("blur", () => {
+        if(formInput.value === ""){
+            thisLabel.classList.remove("active");
+        } 
+     })
+
+
+ });
